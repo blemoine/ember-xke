7903f01afd43fec884bfac5e20529da250e556bc
tchak <paul@chavard.net>
2013-12-13 00:46:06 +0100

    load templates from templates folder

--- js/tutorial/ApplicationWrapper.js
+++ js/tutorial/ApplicationWrapper.js
@@ -1,25 +1,10 @@
+HELPERS = {};
+
 (function() {
   var helper = Ember.Handlebars.helper;
-  window.helpers = {};
 
   Ember.Handlebars.helper= function (name, fn){
-    window.helpers[name] = fn;
+    HELPERS[name] = fn;
     helper(name, fn);
-  };
-
-  var bootstrap = Ember.Handlebars.bootstrap;
-  window.templates = {};
-  Ember.Handlebars.bootstrap = function(ctx) {
-    var selectors = 'script[type="text/x-handlebars"], script[type="text/x-raw-handlebars"]';
-
-    Ember.$(selectors, ctx)
-        .each(function() {
-            var script = Ember.$(this);
-            templateName = script.attr('data-template-name') || script.attr('id') || 'application';
-            if (templateName.indexOf("tutorial") == -1){
-                window.templates[templateName] = script.html().replace(/\s+/g,'');
-            }
-        });
-    bootstrap(ctx);
   };
 })();
