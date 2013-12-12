a60b1af8f615a6d95013514acdb225137dba93f1
tchak <paul@chavard.net>
2013-12-12 19:26:17 +0100

    update to latest ember

--- server.js
+++ server.js
@@ -81,5 +81,6 @@ app.delete('/ponys/:id', function(req, res) {
 });
 
 
-app.listen(3000);
-console.log('Listening on port 3000...');
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Listening on port ' + port + '...');
