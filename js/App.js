e8addda67da9832a744765ac76c191951c108cbc
mbreton <mbreton@xebia.fr>
2013-12-16 22:56:19 +0100

    Update final impl

--- js/App.js
+++ js/App.js
@@ -3,9 +3,10 @@ App = Ember.Application.create({
     LOG_TRANSITIONS:true
 });
 
+
 App.Router.map(function() {
     this.route('add');
-    this.resource('pony', {path: '/:pony_id'},function(){
+    this.resource('pony', {path: '/pony/:pony_id'},function(){
         this.route('edit', {path: '/edit'});
     });
 });
@@ -20,36 +21,63 @@ App.Pony = DS.Model.extend({
     }.property('firstName', 'lastName')
 });
 
+/*
+App.ApplicationAdapter = DS.FixtureAdapter;
+
+App.Pony.FIXTURES = [
+    {
+        id: 1,
+        firstName: 'Rainbow',
+        lastName: 'Dash',
+        color: 'Sky blue',
+        type: 'Pegasus'
+    },
+    {
+        id: 2,
+        firstName: 'Twillight',
+        lastName: 'Sparkle',
+        color: 'Lavender',
+        type: 'Alicorn'
+    },
+    {
+        id: 3,
+        firstName: 'Apple',
+        lastName: 'Jack',
+        color: 'orange',
+        type: 'Earth Pony'
+    }
+];*/
+
 App.IndexRoute = Ember.Route.extend({
     model: function() {
         return this.store.find('pony');
-    },
+    }
+});
+
+App.AddController = Em.ObjectController.extend({
+    actions:{
+        savePony: function() {
+            this.content.save();
+            this.transitionToRoute('pony.index');
+        }
+    }
+});
+
+App.AddRoute = Ember.Route.extend({
+    model: function() {
+        return this.store.createRecord('pony');
+    }
+});
+
+App.IndexController = Em.ArrayController.extend({
+    sortBy: ['name'],
+    sorted: Em.computed.sort('@this', 'sortBy'),
     actions:{
         deletePony: function(pony) {
             pony.deleteRecord();
             pony.save();
         }
     }
-});
-
-App.PonyAddRoute = Ember.Route.extend({
-    model: function() {
-        return this.store.createRecord('pony');
-    }
-});
-
-App.AddRoute = Ember.Route.extend({
-    actions: {
-        savePony: function(pony) {
-            pony.save();
-            this.transitionTo('pony.index');
-        }
-    }
-});
-
-App.IndexController = Em.ArrayController.extend({
-    sortBy: ['name'],
-    sorted: Em.computed.sort('@this', 'sortBy')
 });
 
 
